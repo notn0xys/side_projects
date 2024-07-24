19fa5eecf898f3d69a47deec9c8e504f6d0d53b6
Noxy 3 <thatguynoxxie@gmail.com>
2024-07-24 15:51:19 +0700

    ok

---
+++ Matrix_with_input/target/debug/deps/test2.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\new\Matrix_with_input\target\debug\deps\test2.exe: src/main.rs
+
+C:\Users\user\Documents\GitHub\new\Matrix_with_input\target\debug\deps\test2.d: src/main.rs
+
+src/main.rs:
--- Matrix_with_input/target/debug/test2.d
+++ Matrix_with_input/target/debug/test2.d
@@ -1,1 +1,1 @@
-/home/noxy/Desktop/Code/new/Matrix_with_input/target/debug/test2: /home/noxy/Desktop/Code/new/Matrix_with_input/src/main.rs
+C:\Users\user\Documents\GitHub\new\Matrix_with_input\target\debug\test2.exe: C:\Users\user\Documents\GitHub\new\Matrix_with_input\src\main.rs
