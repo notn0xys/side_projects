ff3d10fd9a9541c602aa167679e10b2ce68faea0
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-01 17:27:33 +0700

    wwawaw

---
+++ Hw/HW2.py
@@ -0,0 +1,2 @@
+x = input("Enter 4 digit integer number");
+reverse(x)
---
+++ Hw/Hw1.py
@@ -0,0 +1,7 @@
+name = input("Enter Employee's name")
+no_of_hours = input("Enter Employee's name")
+pay_rate = float(input("Enter Employee's name"))
+tax_withholding_rate = float(input("Enter Employee's name"))
+state_tax_withholding = float(input("Enter Employee's name"))
+
+
