e77ff0ca0d4b8f659b007bfc513d8d294c216ab2
Noxy 3 <thatguynoxxie@gmail.com>
2024-08-01 19:07:59 +0700

    we

--- Hw/HW2.py
+++ Hw/HW2.py
@@ -1,2 +1,11 @@
-x = input("Enter 4 digit integer number");
-reverse(x)
+while True:
+    x = input("Enter Your input: ")
+    if len(x) == 4:
+        if x.isdigit():
+            x = x[::-1]
+            x = int(x)
+            break
+        print("Not an integer")
+    print("Not 4 characters long")
+print(x)
+
--- Hw/Hw1.py
+++ Hw/Hw1.py
@@ -1,7 +1,19 @@
 name = input("Enter Employee's name")
-no_of_hours = input("Enter Employee's name")
+no_of_hours = int(input("Enter Employee's name"))
 pay_rate = float(input("Enter Employee's name"))
 tax_withholding_rate = float(input("Enter Employee's name"))
 state_tax_withholding = float(input("Enter Employee's name"))
 
+grosspay = no_of_hours * pay_rate
+fed_taxed = grosspay * (1-tax_withholding_rate)
+state_taxed = grosspay * (1-state_tax_withholding)
+total_taxxed = fed_taxed + state_taxed
+net_pay = grosspay - total_taxxed
+
+print("Employee's Name: " + name)
+print("Hours worked: ")
+print("Pay Rate: " + pay_rate)
+print("Gross Pay: {grosspay}")
+print("Deductions:")
+print("  ")
 
